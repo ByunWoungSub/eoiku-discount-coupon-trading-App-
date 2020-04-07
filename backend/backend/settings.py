d719369e26ffc2f558f941155e8d2908baeb9c2f
seojinsarejj <s19009@naver.com>
2020-04-07 19:40:25 +0900

    [UPDATE] update settings.py media_root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'eoiku',
     'rest_framework',
     'corsheaders'
@@ -141,3 +141,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/res'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'coupon')
