b24e56a2671d28bec66ba50e28799f517e56fe56
seojinsarejj <s19009@naver.com>
2020-04-07 19:39:00 +0900

    [CREATE] Create models

--- backend/eoiku/models.py
+++ backend/eoiku/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+
+class User_info(models.Model):
+    
+    id = models.CharField(max_length=30,primary_key=True)
+    passwd = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=20,unique=True)
+    email = models.CharField(max_length=50)
+    phonenum = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.nickname
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User_info,on_delete=models.CASCADE,related_name='name')
+    content = models.TextField()
+    coupon = models.ImageField(upload_to = '{title}')
+    created = models.DateTimeField(auto_now_add=True)
+    state = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['created']
