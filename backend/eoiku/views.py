aa309012146ff6766ef1570f94ecf623991856fb
seojinsarejj <s19009@naver.com>
2020-04-08 19:49:13 +0900

    [UPDATE]Update views

--- backend/eoiku/views.py
+++ backend/eoiku/views.py
@@ -1,18 +1,37 @@
 from django.shortcuts import render,get_object_or_404
-from rest_framework import viewsets
+from rest_framework import generics
 from .models import Post,User_info,Coupon
 from .serializers import *
 # Create your views here.
 
 
-class PostViewSet(viewsets.ModelViewSet):
+#Post
+class PostListView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User_info.objects.all()
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class CouponPostViewSet(viewsets.ModelViewSet):
+
+#User
+class UserListView(generics.ListCreateAPIView):
+    queryset = User_info.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User_info.objects.all()
+    serializer_class = UserSerializer
+
+#Coupon
+class CouponListView(generics.ListCreateAPIView):
     queryset = Coupon.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = CouponSerializer
+
+
+class CouponDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Coupon.objects.all()
+    serializer_class = CouponSerializer
