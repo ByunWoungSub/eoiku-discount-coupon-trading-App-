8a8e2db08afef3a0fd07b9a21a9641382845c5ad
seojinsarejj <s19009@naver.com>
2020-04-08 16:41:53 +0900

    [CREATE] Create PostViewSet

---
+++ backend/eoiku/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path,include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('viewset',views.PostViewSet)
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
+
+
+
+
+
--- backend/eoiku/views.py
+++ backend/eoiku/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render,get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from .models import Post,User_info,Coupon
 from .serializers import *
 # Create your views here.
 
 
-class PostListAPIView(APIView):
-    def get(self, request):
-        serializer = PostSerializer(Post.objects.all(),many=True)
-    
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=201)
-        return Response(serializer.errors, status=400)
-
-
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
