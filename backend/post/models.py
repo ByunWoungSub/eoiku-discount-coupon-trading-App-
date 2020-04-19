bd795d025af41bd2c02a96b32c9c217c662e034c
seojinsarejj <s19009@naver.com>
2020-04-19 15:08:46 +0900

    [UPDATE]Update post app

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 import sys
 sys.path.append("..")
+from account.models import User
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    image = models.ImageField(upload_to='%Y/%m/%d')
+    category = models.CharField(max_length=10,default="")
+    period = models.DateField()
+    price = models.IntegerField(default=0)
+    state = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['created']
+        
--- backend/post/views.py
+++ backend/post/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from rest_framework.views import APIView
+from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 from django.http import JsonResponse
@@ -7,35 +7,10 @@ from rest_framework.response import Response
 # Create your views here.
 
 
-class PostListAPIView(APIView):
-    def get(self,request):
-        serializer = PostSerializer(Post.objects.all(),many=True)
-    def post(self,request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=200)
-        return Response(serializer.errors,staus=400)
+class PostListAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-
-class PostDetailAPIView(APIView):
-    def get_object(self,pk):
-        return get_object_or_404(Post, pk=pk)
-
-    def get(self,request,pk,format=None):
-        post = self.get_object(pk)
-        serializer=PostSerializer(post)
-        return Response(serializer.data)
-
-    def put(self,request,pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors,status=400)
-    
-    def delete(self,request,pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=204)
+class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
